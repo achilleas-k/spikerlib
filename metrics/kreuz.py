f30c74279722d3167295454618e86dc17cabda7a
Achilleas <Achilleas@Morgana>
2014-02-01 20:32:11 +0200

    MP pairwise for Kreuz

--- metrics/kreuz.py
+++ metrics/kreuz.py
@@ -1,13 +1,21 @@
-'''
+"""
+Modifications made by Achilleas Koutsou. List follows:
+
+- Original functions renamed to `distance` and `pairwise` to comply with the
+other modules in spikerlib.
+
+- Added `pairwise_mp` and `interval` functions. See individual
+function docstrings for description.
+
+
 Kreuz SPIKE-distance
 Kreuz, Chicharro, Greschner, Andrzejak, 2011, Journal of Neuroscience Methods.
 and
 Kreuz T, Chicharro D, Houghton C, Andrzejak RG, Mormann F, 2013,
 Journal of Neurophysiology.
 
-All function relating to the SPIKE-distance, except the `interval` function
-were taken from the website mentioned in the comment below.
 
+######################## Original documentation follows #######################
 
 Comment by Thomas Kreuz:
 
@@ -22,7 +30,7 @@ J Neurophysiol 109, 1457-1472 (2013).
 
 The Matlab codes as well as more information can be found at http://www.fi.isc.cnr.it/users/thomas.kreuz/sourcecode.html.
 
-'''
+"""
 import numpy as np
 import multiprocessing
 import itertools
@@ -30,10 +38,10 @@ import itertools
 
 
 def _find_corner_spikes(t, train, ibegin, ti, te):
-    '''
+    """
     Return the times (t1,t2) of the spikes in train[ibegin:]
     such that t1 < t and t2 >= t
-    '''
+    """
     if(ibegin == 0):
         tprev = ti
     else:
@@ -45,12 +53,12 @@ def _find_corner_spikes(t, train, ibegin, ti, te):
     return np.array([train[-1],te]), idts+ibegin
 
 
-def bivariate_spike_distance(t1, t2, ti, te, N):
-    '''Computes the bivariate SPIKE distance of Kreuz et al. (2012)
+def distance(t1, t2, ti, te, N):
+    """Computes the bivariate SPIKE distance of Kreuz et al. (2012)
        t1 and t2 are 1D arrays with the spiking times of two neurones
        It returns the array of the values of the distance
        between time ti and te with N samples.
-       The arrays t1, t2 and values ti, te are unit less '''
+       The arrays t1, t2 and values ti, te are unit less """
     t = np.linspace(ti+(te-ti)/N, te, N)
     d = np.zeros(t.shape)
 
@@ -118,8 +126,8 @@ def bivariate_spike_distance(t1, t2, ti, te, N):
     return t,d
 
 
-def multivariate_spike_distance(spike_trains, ti, te, N):
-    '''
+def pairwise(spike_trains, ti, te, N):
+    """
     t is an array of spike time arrays
     ti the initial time of the recordings
     te the end time of the recordings
@@ -127,14 +135,13 @@ def multivariate_spike_distance(spike_trains, ti, te, N):
     spike_trains is a list of arrays of shape (N, T) with N spike trains
     The multivariate distance is the instantaneous average over all the
     pairwise distances
-    '''
-    # TODO: MP version of this function
+    """
     d = np.zeros((N,))
     n_trains = len(spike_trains)
     t = 0
     for i, t1 in enumerate(spike_trains[:-1]):
         for t2 in spike_trains[i+1:]:
-            tij, dij = bivariate_spike_distance(t1, t2, ti, te, N)
+            tij, dij = distance(t1, t2, ti, te, N)
             if(i == 0):
                 t = tij # The times are only dependent on ti, te, and N
             d = d + dij
@@ -142,24 +149,76 @@ def multivariate_spike_distance(spike_trains, ti, te, N):
     return t,d
 
 
-def interval(inputspikes, outputspikes):
-    '''
+def pairwise_mp(spiketrains, ti, te, N):
+    """
+    Calculates the multivariate distance (the instantaneous average over all
+    the pairwise distances) using Python's multiprocessing.Pool() to
+    run sets of calculations in parallel.
+
+    Arguments have the same meaning as for `pairwise`.
+
+    NB: This function has a slight (on the order 1E-16) deviation from the
+        single-process version of the function ``pairwise()``. The cause of
+        the difference has yet to be determined.
+    """
+    count = len(spiketrains)
+    idx_all = range(count-1)
+    pool = multiprocessing.Pool()
+    distances_nested = pool.map(_all_dist_to_end,
+                                zip(idx_all, itertools.repeat(spiketrains),
+                                    itertools.repeat(ti),
+                                    itertools.repeat(te),
+                                    itertools.repeat(N)))
+    distances = []
+    pool.close()
+    pool.join()
+    times = distances_nested[0][0]
+    for dn in distances_nested:
+        distances.extend(dn[1])
+    print(len(distances))
+    return times, np.mean(distances)
+
+
+def _all_dist_to_end(args):
+    """
+    Helper function for parallel pairwise distance calculations.
+    """
+    idx = args[0]
+    spiketrains = args[1]
+    start = args[2]
+    end = args[3]
+    N = args[4]
+    num_spiketrains = len(spiketrains)
+    distances = []
+    for jdx in range(idx+1, num_spiketrains):
+        dist = distance(spiketrains[idx], spiketrains[jdx], start, end, N)
+        times = dist[0]  # should be the same for all
+        distances.append(dist[1])
+    return times, distances
+
+
+def interval(inputspikes, outputspikes, mp=True):
+    """
     Calculates the mean pairwise SPIKE-distance in intervals defined
     by a separate spike train. This function is used to calculate the distance
     between *input* spike trains based on the interspike intervals of the
     *output* spike train. The result is therefore the distance between the
     input spikes that caused each response.
 
-    inputspikes     - A set of spike trains whose pairwise distance will be
+    inputspikes     A set of spike trains whose pairwise distance will be
                     calculated
 
-    outputspikes    - A single spike train to be used to calculate the
+    outputspikes    A single spike train to be used to calculate the
                     intervals
 
-    '''
+    mp              Set to True to use the multiprocessing implementation
+                    of the pairwise calculation function or False to use the
+                    single process version (default: True)
+    """
     krdists = []
+    pairwise_func = pairwise_mp if mp else pairwise
     for prv, nxt in zip(outputspikes[:-1], outputspikes[1:]):
-        krd = multivariate_spike_distance(inputspikes, prv, nxt, 1)
+        krd = pairwise_func(inputspikes, prv, nxt, 1)
         krdists.append(krd[1])
     return krdists
 
